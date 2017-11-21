e32bdc7734c370c71fef779b12176427da5cdbb8
Kristen Mills <kristen.mills@postmates.com>
2017-11-20 18:40:42 -0800

    another fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 if git log origin/master..origin/develop|grep '^commit '; then
   # Bump the current release version
-  VERSION=$(npm run release:bump -- release)
+  VERSION=$(npm run release:bump --silent -- release)
 
   # Create the current release
   git add package.json
@@ -10,7 +10,7 @@ if git log origin/master..origin/develop|grep '^commit '; then
   git tag -a -f "$VERSION" -m "$VERSION release"
 
   # Create dev release
-  DEV_VERSION=$(npm run release:bump -- dev)
+  DEV_VERSION=$(npm run release:bump --silent -- dev)
   git add package.json
   git commit -m "Prepare v$DEV_VERSION"
 
