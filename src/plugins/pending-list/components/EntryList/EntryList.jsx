b2cd276d8e168a41b568cff5da142dd606c0fe42
Steve Adams <stevezau@gmail.com>
2018-02-24 14:44:21 +1100

    add filters

--- src/plugins/pending-list/components/EntryList/EntryList.jsx
+++ src/plugins/pending-list/components/EntryList/EntryList.jsx
@@ -1,6 +1,10 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import FlexGetEntry from 'common/FlexGetEntry';
+import Select from 'material-ui/Select';
+import { MenuItem } from 'material-ui/Menu';
+import { InputLabel } from 'material-ui/Input';
+import { FormControl, FormHelperText } from 'material-ui/Form';
 import Entry from '../Entry';
 import AddEntryDialog from '../AddEntryDialog';
 import {
@@ -28,17 +32,28 @@ export default class EntryList extends PureComponent {
 
   state = {
     entryModalOpen: false,
+    sortBy: 'added',
+    sortOrder: 'desc',
   }
 
   componentDidMount() {
-    this.props.getEntries();
+    const { sortBy, sortOrder } = this.state;
+    this.props.getEntries({ sort_by: sortBy, order: sortOrder });
   }
 
   componentWillReceiveProps({ listId, getEntries }) {
+    const { sortBy, sortOrder } = this.state;
     if (this.props.listId !== listId) {
-      getEntries();
+      getEntries({ sort_by: sortBy, order: sortOrder });
     }
   }
+
+  handleChange = (event, ele) => {
+    console.log(ele);
+    console.log(event);
+    console.log({ [event.target.name]: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   openRemoveEntryModal = entry => () => this.setState({ removeEntry: entry })
   openAddEntryModal = () => this.setState({ entryModalOpen: true })
@@ -46,7 +61,7 @@ export default class EntryList extends PureComponent {
 
   render() {
     const { entries: { items }, listId } = this.props;
-    const { entryModalOpen } = this.state;
+    const { entryModalOpen, sortBy, sortOrder } = this.state;
 
     if (!listId) {
       return null;
@@ -54,16 +69,48 @@ export default class EntryList extends PureComponent {
 
     return (
       <div>
-        <ButtonWrapper>
-          <AddEntryButton
-            color="primary"
-            size="small"
-            onClick={this.openAddEntryModal}
-          >
-            <Icon icon="plus-circle" />
-            Add Entry
-          </AddEntryButton>
-        </ButtonWrapper>
+        <form autoComplete="off">
+          <ButtonWrapper>
+            <FormControl>
+              <InputLabel htmlFor="sort-by">Sort</InputLabel>
+              <Select
+                value={sortBy}
+                onChange={this.handleChange}
+                inputProps={{
+                  name: 'sortBy',
+                  id: 'sortBy',
+                }}
+              >
+                <MenuItem value={'added'}>Date Added</MenuItem>
+                <MenuItem value={'title'}>Title</MenuItem>
+                <MenuItem value={'original_url'}>URL</MenuItem>
+                <MenuItem value={'approved'}>Approved</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="order-by">Order</InputLabel>
+              <Select
+                value={sortOrder}
+                onChange={this.handleChange}
+                inputProps={{
+                  name: 'sortOrder',
+                  id: 'sortOrder',
+                }}
+              >
+                <MenuItem value={'desc'}>Desc</MenuItem>
+                <MenuItem value={'asc'}>Asc</MenuItem>
+              </Select>
+            </FormControl>
+            <AddEntryButton
+              color="primary"
+              size="small"
+              onClick={this.openAddEntryModal}
+            >
+              <Icon icon="plus-circle" />
+              Add Entry
+            </AddEntryButton>
+          </ButtonWrapper>
+        </form>
         <ListWrapper>
           {items && items.map(entry => (
             <EntryWrapper key={entry.id}>
@@ -80,6 +127,7 @@ export default class EntryList extends PureComponent {
           listId={listId}
         />
       </div>
-    );
+    )
+      ;
   }
 }
