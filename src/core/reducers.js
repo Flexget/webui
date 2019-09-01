821476ea60e991d387d4c80d56076941d587b28d
Kristen Mills <kristen@kristen-mills.com>
2019-09-01 10:37:20 -0700

    [Fix] Fix connected router version and tablist issues (#34)

--- src/core/reducers.js
+++ src/core/reducers.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+import history from 'core/history';
+import { connectRouter } from 'connected-react-router';
 import auth from 'core/auth/state/reducer';
 import routes from 'core/routes/state/reducer';
 import status from 'core/status/state/reducer';
 import tasks from 'core/tasks/state/reducer';
 import version from 'core/version/state/reducer';
 
-export default function createReducers(reducers) {
+export default function createReducers(reducers = {}) {
   return combineReducers({
     auth,
     form,
@@ -14,6 +16,7 @@ export default function createReducers(reducers) {
     status,
     version,
     tasks,
+    router: connectRouter(history),
     ...reducers,
   });
 }
