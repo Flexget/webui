b854a4cf338602e3b66a93b9c1f68a0dbb71cdef
Kristen Mills <kristen@kristen-mills.com>
2019-12-04 14:55:11 -0800

    fix test

--- src/core/routes/PrivateRoute/PrivateRoute.spec.jsx
+++ src/core/routes/PrivateRoute/PrivateRoute.spec.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { mapStateToProps, PrivateRoute } from 'core/routes/PrivateRoute';
 import renderer from 'react-test-renderer';
-import { router } from 'utils/tests';
+import { router, authProvider } from 'utils/tests';
 
 const Component = () => <div />;
 
 describe('core/routes/components/PrivateRoute', () => {
   describe('PrivateRoute', () => {
     it('renders correctly when logged in', () => {
-      const privateRoute = <PrivateRoute component={Component} loggedIn />;
-      const tree = renderer.create(router(privateRoute)).toJSON();
+      const privateRoute = <PrivateRoute component={Component} />;
+      const tree = renderer.create(authProvider(router(privateRoute), true)).toJSON();
       expect(tree).toMatchSnapshot();
     });
 
     xit('renders correctly when logged out', () => {
-      const privateRoute = <PrivateRoute component={Component} loggedIn={false} />;
-      const tree = renderer.create(router(privateRoute)).toJSON();
+      const privateRoute = <PrivateRoute component={Component} />;
+      const tree = renderer.create(authProvider(router(privateRoute))).toJSON();
       expect(tree).toMatchSnapshot();
     });
   });
