c4750c5c7b4f4b4fe710e68701ab0a7bb2674342
Kristen Mills <kristen@kristen-mills.com>
2019-12-14 19:16:11 -0800

    tests

--- src/core/layout/Navbar/Navbar.spec.tsx
+++ src/core/layout/Navbar/Navbar.spec.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { IconButton } from '@material-ui/core';
 import { AuthContainer } from 'core/auth/container';
 import { StatusContainer } from 'core/status/hooks';
 import Navbar from './index';
 import { NavbarContainer } from './hooks';
 
-const renderNavbar = (toggle: () => void) => (
+const TestNavbar: FC<{ toggle: () => void }> = ({ toggle }) => (
   <StatusContainer.Provider>
     <AuthContainer.Provider>
       <MemoryRouter>
@@ -20,32 +19,15 @@ const renderNavbar = (toggle: () => void) => (
   </StatusContainer.Provider>
 );
 
-describe('core/layout/components/Navbar', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(renderNavbar(jest.fn())).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  xit('calls toggle when the hamburger button is pressed', () => {
+describe('core/layout/Navbar', () => {
+  it('calls toggle when the hamburger button is pressed', () => {
     const toggle = jest.fn();
-    const wrapper = shallow(<Navbar toggleSidebar={toggle} />);
+    const wrapper = mount(<TestNavbar toggle={toggle} />);
 
     wrapper
-      .find('.fa-bars')
-      .closest(IconButton)
+      .findWhere(el => el.props()['aria-label'] === 'toggle sidebar' && el.type() === IconButton)
       .simulate('click');
+
     expect(toggle).toHaveBeenCalled();
-  });
-
-  xit('calls handleMenuClick when the menu button is pressed', () => {
-    const toggle = jest.fn();
-    const target = { a: 'target' };
-    const wrapper = shallow(<Navbar toggleSidebar={toggle} />);
-
-    const button = wrapper.find('.fa-cog').closest(IconButton);
-
-    button.simulate('click', { currentTarget: target });
-    // expect(wrapper.state().menuOpen).toEqual(true);
-    // expect(wrapper.state().anchorEl).toEqual(target);
   });
 });
