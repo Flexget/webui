94c1cb16fa4b9fe6748537c5f9d84e1ab83f1e33
Kristen Mills <kristen@kristen-mills.com>
2019-12-29 19:59:29 -0800

    fix

--- src/plugins/log/Header.spec.tsx
+++ src/plugins/log/Header.spec.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
+import { cleanup } from '@testing-library/react';
 import renderer from 'react-test-renderer';
+import fetchMock from 'fetch-mock';
 import { themed } from 'utils/tests';
 import { ReadyState } from 'core/api';
 import Header from './Header';
 
-describe('plugins/log/components/Header', () => {
-  describe('Header', () => {
-    it('renders correctly', () => {
-      const tree = renderer
-        .create(
-          themed(
-            <Header
-              connect={jest.fn()}
-              readyState={ReadyState.Open}
-              disconnect={jest.fn()}
-              options={{ query: '', lines: 400 }}
-              setOptions={jest.fn()}
-              clear={jest.fn()}
-            />,
-          ),
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
-    });
+describe('plugins/log/Header', () => {
+  beforeEach(() => {
+    fetchMock.get('/api/tasks', []).catch();
+  });
+
+  afterEach(() => {
+    cleanup();
+    fetchMock.reset();
+  });
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        themed(
+          <Header
+            connect={jest.fn()}
+            readyState={ReadyState.Open}
+            disconnect={jest.fn()}
+            options={{ query: '', lines: 400 }}
+            setOptions={jest.fn()}
+            clear={jest.fn()}
+          />,
+        ),
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
