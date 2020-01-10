c512134bf253cd37fb344852779a4317cabe0fc9
Kristen Mills <kristen@kristen-mills.com>
2020-01-10 13:23:43 -0800

    [fix] make the question mark show properly in the version section of the sidebar

--- src/core/layout/SideNav/Version.spec.tsx
+++ src/core/layout/SideNav/Version.spec.tsx
@@ -1,50 +1,67 @@
 import React from 'react';
 import fetchMock from 'fetch-mock';
-import { act, create, ReactTestRenderer } from 'react-test-renderer';
-import { StatusContainer } from 'core/status/hooks';
-import { AuthContainer } from 'core/auth/container';
+import { cleanup } from '@testing-library/react';
+import { renderWithWrapper } from 'utils/tests';
 import Version from './Version';
 import { VersionContainer } from './hooks';
 
 describe('core/layout/Version', () => {
   beforeEach(() => {
+    fetchMock.get('/api/tasks', []).catch();
+  });
+
+  afterEach(() => {
+    cleanup();
     fetchMock.reset();
   });
+
   const component = (
-    <StatusContainer.Provider>
-      <AuthContainer.Provider>
-        <VersionContainer.Provider>
-          <Version />
-        </VersionContainer.Provider>
-      </AuthContainer.Provider>
-    </StatusContainer.Provider>
+    <VersionContainer.Provider>
+      <Version />
+    </VersionContainer.Provider>
   );
-  it('renders correctly with latest version', async () => {
+
+  it('does not show icon with latest version', async () => {
     fetchMock.get('/api/server/version', {
       apiVersion: '1.1.2',
       flexgetVersion: '2.10.60',
       latestVersion: '2.10.60',
     });
-    let tree: ReactTestRenderer | undefined;
-    await act(() => {
-      tree = create(component);
-      return Promise.resolve();
-    });
-    expect(tree?.toJSON()).toMatchSnapshot();
+    const { findByText, queryByLabelText } = renderWithWrapper(component);
+    expect(await findByText('Flexget: 2.10.60')).toBeInTheDocument();
+    expect(queryByLabelText('flexget update available')).not.toBeInTheDocument();
   });
 
-  it('renders correctly without latest version', async () => {
+  it('does not show icon with latest dev version', async () => {
+    fetchMock.get('/api/server/version', {
+      apiVersion: '1.1.2',
+      flexgetVersion: '2.10.61.dev',
+      latestVersion: '2.10.60',
+    });
+    const { findByText, queryByLabelText } = renderWithWrapper(component);
+    expect(await findByText('Flexget: 2.10.61.dev')).toBeInTheDocument();
+    expect(queryByLabelText('flexget update available')).not.toBeInTheDocument();
+  });
+
+  it('does show icon with a previous dev version', async () => {
+    fetchMock.get('/api/server/version', {
+      apiVersion: '1.1.2',
+      flexgetVersion: '2.10.60.dev',
+      latestVersion: '2.10.60',
+    });
+    const { findByText, queryByLabelText } = renderWithWrapper(component);
+    expect(await findByText('Flexget: 2.10.60.dev')).toBeInTheDocument();
+    expect(queryByLabelText('flexget update available')).toBeInTheDocument();
+  });
+
+  it('does show the icon with an old version', async () => {
     fetchMock.get('/api/server/version', {
       apiVersion: '1.1.2',
       flexgetVersion: '2.10.11',
       latestVersion: '2.10.60',
     });
-
-    let wrapper: ReactTestRenderer | undefined;
-    await act(() => {
-      wrapper = create(component);
-      return Promise.resolve();
-    });
-    expect(wrapper?.toJSON()).toMatchSnapshot();
+    const { findByText, queryByLabelText } = renderWithWrapper(component);
+    expect(await findByText('Flexget: 2.10.11')).toBeInTheDocument();
+    expect(queryByLabelText('flexget update available')).toBeInTheDocument();
   });
 });
