ee7ac4a335411518211b95071d9dde9ad1e8d6c4
Kristen Mills <kristen@kristen-mills.com>
2020-01-28 19:01:01 -0800

    more wip

--- src/plugins/tasks/ExecuteDialog.tsx
+++ src/plugins/tasks/ExecuteDialog.tsx
@@ -1,7 +1,15 @@
 import React, { FC, useEffect } from 'react';
 import { useContainer } from 'unstated-next';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
-import { Form, useFormikContext } from 'formik';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Checkbox,
+  FormControlLabel,
+} from '@material-ui/core';
+import { Form, useFormikContext, Field } from 'formik';
 import Autocomplete from 'common/inputs/formik/Autocomplete';
 import { TaskContainer } from 'plugins/tasks/hooks';
 import { useOverlayState } from 'utils/hooks';
@@ -16,24 +24,27 @@ const ExecuteDialog: FC<Props> = ({ readyState }) => {
   const { tasks } = useContainer(TaskContainer);
   const [isOpen, { open, close }] = useOverlayState();
 
-  const { isSubmitting, resetForm } = useFormikContext<ExecuteTaskRequest>();
+  const { setSubmitting, isSubmitting, resetForm } = useFormikContext<ExecuteTaskRequest>();
   useEffect(() => {
     if (isSubmitting && readyState === ReadyState.Open) {
       resetForm();
       close();
+      setSubmitting(false);
     }
-  }, [close, isSubmitting, readyState, resetForm]);
+  }, [close, isSubmitting, readyState, resetForm, setSubmitting]);
 
   return (
     <>
-      <Button onClick={open}>Execute</Button>
+      <Button onClick={open} color="primary">
+        Execute
+      </Button>
       <Dialog open={isOpen} onClose={close} fullWidth maxWidth="xs">
         <DialogTitle>Execute Tasks</DialogTitle>
         <Form>
           <DialogContent>
             <Autocomplete
               multiple
-              options={tasks}
+              options={tasks.map(t => t.name)}
               getOptionLabel={option => option}
               filterSelectedOptions
               name="tasks"
@@ -41,6 +52,20 @@ const ExecuteDialog: FC<Props> = ({ readyState }) => {
                 label: 'Task(s)',
               }}
             />
+            <FormControlLabel
+              control={<Field name="noCache" component={Checkbox} />}
+              label="No Cache"
+            />
+            <FormControlLabel
+              control={<Field name="disableTracking" component={Checkbox} />}
+              label="Disable Tracking"
+            />
+            <FormControlLabel
+              control={<Field name="discoverNow" component={Checkbox} />}
+              label="Discover Now"
+            />
+            <FormControlLabel control={<Field name="learn" component={Checkbox} />} label="Learn" />
+            <FormControlLabel control={<Field name="now" component={Checkbox} />} label="Now" />
           </DialogContent>
           <DialogActions>
             <Button onClick={close} type="button">
