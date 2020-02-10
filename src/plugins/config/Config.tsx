852299ba18dee9256e78106342e9e507d7dcc9f3
Levi <shadycuz@gmail.com>
2020-02-10 12:22:23 +0530

    Feature: improve messaging (#93)
    * fixed a typo
    
    * add paragraph describing variables and there use
    
    * update snapshot with typo fix
    
    * escape a single quote
    
    * remove inline css and use Link component
    
    * add instructions to lint and test code
    
    * removed extra div
    
    * fix variable description spacing

--- src/plugins/config/Config.tsx
+++ src/plugins/config/Config.tsx
@@ -2,7 +2,7 @@ import React, { useState, useCallback, useMemo, FC } from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Formik } from 'formik';
 import { useInjectPageTitle } from 'core/layout/AppBar/hooks';
-import { Button, Theme } from '@material-ui/core';
+import { Button, Theme, Link } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 import { useGlobalStatus } from 'core/status/hooks';
 import { NoPaddingWrapper } from 'common/styles';
@@ -22,6 +22,12 @@ const toolbar = (theme: Theme) => css`
   align-items: center;
   padding: ${theme.typography.pxToRem(theme.spacing(2))}
     ${theme.typography.pxToRem(theme.spacing(4))};
+`;
+
+const varDescription = (theme: Theme) => css`
+  margin: ${theme.typography.pxToRem(theme.spacing(2))}
+    ${theme.typography.pxToRem(theme.spacing(4))};
+  margin-bottom: 0;
 `;
 
 const button = (theme: Theme) => css`
@@ -77,6 +83,12 @@ const Config: FC = () => {
     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       <NoPaddingWrapper>
         <ResetForm initialValues={initialValues} />
+        <p css={varDescription}>
+          Variables allow you reuse sections of your config file. They are also useful for hiding
+          sensitive information like passwords.
+          <br /> Any variables you create in the WebUI are saved in the DB and not to a file. You
+          can find out more <Link href="https://flexget.com/Plugins/variables">here</Link>.
+        </p>
         <div css={toolbar}>
           <div>
             <Button
